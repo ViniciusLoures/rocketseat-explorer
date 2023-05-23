a2a4b458fbbb628c4ab50a89e31ef683c7716242
Vinicius Alves Loures <vinicius.loures@radixeng.com.br>
2023-05-23 10:44:35 -0300

    First Commit

---
+++ focus-timer copy/js/elements.js
@@ -0,0 +1,20 @@
+const btnPause = document.querySelector('.pause');
+const btnPlay = document.querySelector('.play');
+const btnStop = document.querySelector('.stop');
+const btnSet = document.querySelector('.set');
+const btnSoundOn = document.querySelector('.sound-on');
+const btnSoundOff = document.querySelector('.sound-off');
+
+const minutesDisplay = document.querySelector('.minutes');
+const secondsDisplay = document.querySelector('.seconds');
+
+export const elements = {
+  btnPause,
+  btnPlay,
+  btnStop,
+  btnSet,
+  btnSoundOn,
+  btnSoundOff,
+  minutesDisplay,
+  secondsDisplay,
+}
